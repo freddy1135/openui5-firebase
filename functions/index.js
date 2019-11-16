5f5ff673702500e78e154b736f4698cd2e43edb8
Freddy Jetty Johnson <freddy.jetty.johnson@sap.com>
2019-11-16 23:39:39 +0530

    SampleApp - Email not working

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,23 @@
 let functions = require('firebase-functions');
 let express = require('express');
 let app = express();
+let sgMail = require('@sendgrid/mail');
 
+app.post('/sendEmail', function (req, res) {
+    console.log("%j", req.body);
+        sgMail.setApiKey('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');
+        var msg = {
+        to: req.body.email,
+        from: 'freddy.jetty.johnson@sap.com',
+        subject: 'Sending with SendGrid is Fun',
+        text: req.body.message
+    };
+    sgMail.send(msg);
+    console.log('Email Send Successfully!');
+    res.send({
+        "statusCode" : "200",
+        "email" : req.body.email,
+        "message": req.body.message
+    });
+});
 exports.app = functions.https.onRequest(app);
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,37 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], function (Controller, MessageToast) {
+	"sap/m/MessageBox"
+], function (Controller, MessageBox) {
 	"use strict";
+
 	return Controller.extend("openUI5.controller.App", {
-		onPress: function () {
-			MessageToast.show("Hello World!");
+
+		onSendEmail: function (oEvent) {
+
+			var oEntry = {
+				"email": this.byId("email").getValue(),
+				"message": this.byId("message").getValue()
+			}
+			var that = this;
+			$.ajax({
+				type: "POST",
+				url: '/sendEmail',
+				dataType: "json",
+				data: JSON.stringify(oEntry),
+				contentType: "application/json",
+				success: function (data) {
+					MessageBox.success("Email Send Successfully");
+					that.byId("email").setValue("");
+					that.byId("message").setValue("");
+				},
+
+				error: function (err) {
+					console.log(err);
+					MessageBox.error("Error while sending Email");
+				}
+			});
+
 		}
 	});
+
 });
